b277c558977f79bfaf672027d690abca6b1b4795
kaiquelopeslp <kaiquelopeslp@gmail.com>
2024-05-16 19:59:20 -0300

    Primeiro commit

---
+++ aula1.py
@@ -0,0 +1,18 @@
+def boasVindas(nome, idade):
+    print("Olá {}, nem parece que você tem {} anos". format(nome, idade))
+
+def ePar(numero):
+    print(numero%2)
+    if (numero%2)  == 0:
+        print("{} é par".format(numero))
+    else:
+        print("{} é impar".format(numero))
+
+
+
+if __name__== "__main__":
+    #nome = input("Qual seu nome? ")
+    #idade = int(input("Qual sua idade? "))
+    #boasVindas(nome, idade)
+    numero = (int(input("Digite o número: ")))
+    ePar(numero)
